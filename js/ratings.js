4b82b45b60f97197667b170f0a7e0b748d04eb1e
Leonard Collins <leonard@taal.com>
2022-10-06 05:15:59 +0100

    initial commit

--- js/ratings.js
+++ js/ratings.js
@@ -13,4 +13,10 @@ function collect_ratings() {
         ratings.count += parseInt(element.value)
         ratings.sum += parseInt(element.value) * rating
     });
+
+    if (ratings.count !== 0) {
+        ratings.average = ratings.sum / ratings.count
+    }
+
+    return ratings
 }
