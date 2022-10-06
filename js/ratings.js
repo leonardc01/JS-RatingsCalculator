8c5383bbb049af7b346132f25b3f0a7bb99744ef
Leonard Collins <leonard@taal.com>
2022-10-06 05:21:20 +0100

    initial commit

--- js/ratings.js
+++ js/ratings.js
@@ -24,5 +24,5 @@ function collect_ratings() {
 document.addEventListener('change', () => { 
 
     const ratings = collect_ratings()
-    document.querySelector('average').value = ratings.average.toFixed(2)
+    document.querySelector('#average').value = ratings.average.toFixed(2)
 })
